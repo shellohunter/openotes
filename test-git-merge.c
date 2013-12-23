d9b6b0285debf1523621d250ba8dc4f04154d765
shello hunter <shhere@gmail.com>
2013-12-23 23:22:51 +0800

    add a function too

--- test-git-merge.c
+++ test-git-merge.c
@@ -1,5 +1,15 @@
-1aaaaaa
-2bbbbbb
-3aaaaaa
-4dddddd
+1aaaaa
+2bbbbb
+3aaaaa
+4ddddd
+5ccccc
 ===================
+
+
+int this_is_a_test(unsigned int o)
+{
+	int a=0;
+	int b=0;
+
+	return a+b+i;
+}
